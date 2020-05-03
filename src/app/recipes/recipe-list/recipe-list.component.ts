184769dc98a15ddba0f0bc566a63d6f225c5b5a2
anshdeep_chawla <a.chawla@easternenterprise.com>
2020-05-04 04:29:22 +0530

    [TASK] Added navigation with event binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -12,8 +12,12 @@ export class RecipeListComponent implements OnInit {
       'Test recipe', 
     'Test description', 
     'https://images.unsplash.com/photo-1518779578993-ec3579fee39f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80'
-    )
-  ];
+    ),
+    new Recipe(
+      'Test recipe', 
+    'Test description', 
+    'https://images.unsplash.com/photo-1518779578993-ec3579fee39f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80'
+    )];
 
   constructor() { }
 
